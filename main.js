50e2c944a5de59a191b4ae5fe35f38980c6e0992
Moaaz <121523175+moaaztarek623@users.noreply.github.com>
2023-04-25 16:39:32 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,156 @@
+let input_1 = document.querySelector('#input-1');
+let input_2 = document.querySelector('#input-2');
+let arrayOfChars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+let code_1 = document.querySelector('.code-1');
+let code_2 = document.querySelector('.code-2');
+
+input_2.oninput = function () {
+    if (input_2.value == arrayOfChars[0]) {
+        code_2.innerHTML = '901'
+    }
+    if (input_2.value == arrayOfChars[1]) {
+        code_2.innerHTML = '902'
+    }
+    if (input_2.value == arrayOfChars[2]) {
+        code_2.innerHTML = '903'
+    }
+    if (input_2.value == arrayOfChars[3]) {
+        code_2.innerHTML = '904'
+    }
+    if (input_2.value == arrayOfChars[4]) {
+        code_2.innerHTML = '905'
+    }
+    if (input_2.value == arrayOfChars[5]) {
+        code_2.innerHTML = '906'
+    }
+    if (input_2.value == arrayOfChars[6]) {
+        code_2.innerHTML = '907'
+    }
+    if (input_2.value == arrayOfChars[7]) {
+        code_2.innerHTML = '908'
+    }
+    if (input_2.value == arrayOfChars[9]) {
+        code_2.innerHTML = '910'
+    }
+    if (input_2.value == arrayOfChars[10]) {
+        code_2.innerHTML = '911'
+    }
+    if (input_2.value == arrayOfChars[11]) {
+        code_2.innerHTML = '912'
+    }
+    if (input_2.value == arrayOfChars[12]) {
+        code_2.innerHTML = '913'
+    }
+    if (input_2.value == arrayOfChars[13]) {
+        code_2.innerHTML = '914'
+    }
+    if (input_2.value == arrayOfChars[14]) {
+        code_2.innerHTML = '915'
+    }
+    if (input_2.value == arrayOfChars[15]) {
+        code_2.innerHTML = '916'
+    }
+    if (input_2.value == arrayOfChars[16]) {
+        code_2.innerHTML = '917'
+    }
+    if (input_2.value == arrayOfChars[17]) {
+        code_2.innerHTML = '918'
+    }
+    if (input_2.value == arrayOfChars[18]) {
+        code_2.innerHTML = '919'
+    }
+    if (input_2.value == arrayOfChars[19]) {
+        code_2.innerHTML = '920'
+    }
+    if (input_2.value == arrayOfChars[20]) {
+        code_2.innerHTML = '921'
+    }
+    if (input_2.value == arrayOfChars[21]) {
+        code_2.innerHTML = '922'
+    }
+    if (input_2.value == arrayOfChars[22]) {
+        code_2.innerHTML = '923'
+    }
+    if (input_2.value == arrayOfChars[23]) {
+        code_2.innerHTML = '924'
+    }
+    if (input_2.value == arrayOfChars[24]) {
+        code_2.innerHTML = '925'
+    }
+    if (input_2.value == arrayOfChars[25]) {
+        code_2.innerHTML = '926'
+    }
+}
+
+input_1.oninput = function () {
+    if (input_1.value == 901) {
+     code_1.innerHTML = `i love ${arrayOfChars[0]}`    }
+    if (input_1.value == 902) {
+     code_1.innerHTML = `i love ${arrayOfChars[1]}`    }
+    if (input_1.value == 903) {
+     code_1.innerHTML = `i love ${arrayOfChars[2]}`    }
+    if (input_1.value == 904) {
+     code_1.innerHTML = `i love ${arrayOfChars[3]}`    }
+    if (input_1.value == 905) {
+     code_1.innerHTML = `i love ${arrayOfChars[4]}`    }
+    if (input_1.value == 906) {
+     code_1.innerHTML = `i love ${arrayOfChars[5]}`    }
+    if (input_1.value == 907) {
+     code_1.innerHTML = `i love ${arrayOfChars[6]}`    }
+    if (input_1.value == 908) {
+     code_1.innerHTML = `i love ${arrayOfChars[7]}`    }
+    if (input_1.value == 909) {
+     code_1.innerHTML = `i love ${arrayOfChars[8]}`    }
+    if (input_1.value == 910) {
+     code_1.innerHTML = `i love ${arrayOfChars[9]}`    }
+    if (input_1.value == 911) {
+     code_1.innerHTML = `i love ${arrayOfChars[10]}`
+    }
+    if (input_1.value == 912) {
+     code_1.innerHTML = `i love ${arrayOfChars[11]}`
+    }
+    if (input_1.value == 913) {
+     code_1.innerHTML = `i love ${arrayOfChars[12]}`
+    }
+    if (input_1.value == 914) {
+     code_1.innerHTML = `i love ${arrayOfChars[13]}`
+    }
+    if (input_1.value == 915) {
+     code_1.innerHTML = `i love ${arrayOfChars[14]}`
+    }
+    if (input_1.value == 916) {
+     code_1.innerHTML = `i love ${arrayOfChars[15]}`
+    }
+    if (input_1.value == 917) {
+     code_1.innerHTML = `i love ${arrayOfChars[16]}`
+    }
+    if (input_1.value == 918) {
+     code_1.innerHTML = `i love ${arrayOfChars[17]}`
+    }
+    if (input_1.value == 919) {
+     code_1.innerHTML = `i love ${arrayOfChars[18]}`
+    }
+    if (input_1.value == 920) {
+     code_1.innerHTML = `i love ${arrayOfChars[19]}`
+    }
+    if (input_1.value == 921) {
+     code_1.innerHTML = `i love ${arrayOfChars[20]}`
+    }
+    if (input_1.value == 922) {
+    code_1.innerHTML = `i love ${arrayOfChars[21]}`;
+    }
+    if (input_1.value == 923) {
+     code_1.innerHTML = `i love ${arrayOfChars[22]}`
+    }
+    if (input_1.value == 924) {
+     code_1.innerHTML = `i love ${arrayOfChars[23]}`
+    }
+    if (input_1.value == 925) {
+     code_1.innerHTML = `i love ${arrayOfChars[24]}`
+    }
+    if (input_1.value == 926) {
+     code_1.innerHTML = `i love ${arrayOfChars[25]}`
+    }
+    //char
+   
+}
