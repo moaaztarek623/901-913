f6190e7cb40adf508ad0bb8ed3af1f9e20ed12a4
Moaaz <121523175+moaaztarek623@users.noreply.github.com>
2023-04-26 05:23:25 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -3,156 +3,36 @@ let input_2 = document.querySelector('#input-2');
 let arrayOfChars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 let code_1 = document.querySelector('.code-1');
 let code_2 = document.querySelector('.code-2');
-
+let em = [];
+let em_2 = [];
 
 
 input_2.oninput = function () {
-    if (input_2.value == arrayOfChars[0]) {
-        code_2.innerHTML = '901'
-    }
-    if (input_2.value == arrayOfChars[1]) {
-        code_2.innerHTML = '902'
-    }
-    if (input_2.value == arrayOfChars[2]) {
-        code_2.innerHTML = '903'
-    }
-    if (input_2.value == arrayOfChars[3]) {
-        code_2.innerHTML = '904'
-    }
-    if (input_2.value == arrayOfChars[4]) {
-        code_2.innerHTML = '905'
-    }
-    if (input_2.value == arrayOfChars[5]) {
-        code_2.innerHTML = '906'
-    }
-    if (input_2.value == arrayOfChars[6]) {
-        code_2.innerHTML = '907'
-    }
-    if (input_2.value == arrayOfChars[7]) {
-        code_2.innerHTML = '908'
-    }
-    if (input_2.value == arrayOfChars[9]) {
-        code_2.innerHTML = '910'
-    }
-    if (input_2.value == arrayOfChars[10]) {
-        code_2.innerHTML = '911'
-    }
-    if (input_2.value == arrayOfChars[11]) {
-        code_2.innerHTML = '912'
-    }
-    if (input_2.value == arrayOfChars[12]) {
-        code_2.innerHTML = '913'
-    }
-    if (input_2.value == arrayOfChars[13]) {
-        code_2.innerHTML = '914'
-    }
-    if (input_2.value == arrayOfChars[14]) {
-        code_2.innerHTML = '915'
-    }
-    if (input_2.value == arrayOfChars[15]) {
-        code_2.innerHTML = '916'
-    }
-    if (input_2.value == arrayOfChars[16]) {
-        code_2.innerHTML = '917'
-    }
-    if (input_2.value == arrayOfChars[17]) {
-        code_2.innerHTML = '918'
-    }
-    if (input_2.value == arrayOfChars[18]) {
-        code_2.innerHTML = '919'
-    }
-    if (input_2.value == arrayOfChars[19]) {
-        code_2.innerHTML = '920'
-    }
-    if (input_2.value == arrayOfChars[20]) {
-        code_2.innerHTML = '921'
-    }
-    if (input_2.value == arrayOfChars[21]) {
-        code_2.innerHTML = '922'
-    }
-    if (input_2.value == arrayOfChars[22]) {
-        code_2.innerHTML = '923'
-    }
-    if (input_2.value == arrayOfChars[23]) {
-        code_2.innerHTML = '924'
-    }
-    if (input_2.value == arrayOfChars[24]) {
-        code_2.innerHTML = '925'
-    }
-    if (input_2.value == arrayOfChars[25]) {
-        code_2.innerHTML = '926'
-    }
+    for (let i = 901; i <= 926; i++) {
+        (em_2.push(i.toString()))
+        for (let j = 0; j < arrayOfChars.length; j++) {       
+            if (input_2.value == arrayOfChars[j]) {
+                code_2.innerHTML = `${(em_2[j])}`
+            }
+        }
+   }
 }
 
 input_1.oninput = function () {
-    if (input_1.value == 901) {
-     code_1.innerHTML = `i love ${arrayOfChars[0].toUpperCase()}`    }
-    if (input_1.value == 902) {
-     code_1.innerHTML = `i love ${arrayOfChars[1].toUpperCase()}`    }
-    if (input_1.value == 903) {
-     code_1.innerHTML = `i love ${arrayOfChars[2].toUpperCase()}`    }
-    if (input_1.value == 904) {
-     code_1.innerHTML = `i love ${arrayOfChars[3].toUpperCase()}`    }
-    if (input_1.value == 905) {
-     code_1.innerHTML = `i love ${arrayOfChars[4].toUpperCase()}`    }
-    if (input_1.value == 906) {
-     code_1.innerHTML = `i love ${arrayOfChars[5].toUpperCase()}`    }
-    if (input_1.value == 907) {
-     code_1.innerHTML = `i love ${arrayOfChars[6].toUpperCase()}`    }
-    if (input_1.value == 908) {
-     code_1.innerHTML = `i love ${arrayOfChars[7].toUpperCase()}`    }
-    if (input_1.value == 909) {
-     code_1.innerHTML = `i love ${arrayOfChars[8].toUpperCase()}`    }
-    if (input_1.value == 910) {
-     code_1.innerHTML = `i love ${arrayOfChars[9].toUpperCase()}`    }
-    if (input_1.value == 911) {
-     code_1.innerHTML = `i love ${arrayOfChars[10].toUpperCase()}`
-    }
-    if (input_1.value == 912) {
-     code_1.innerHTML = `i love ${arrayOfChars[11].toUpperCase()}`
-    }
-    if (input_1.value == 913) {
-     code_1.innerHTML = `i love ${arrayOfChars[12].toUpperCase()}`
-    }
-    if (input_1.value == 914) {
-     code_1.innerHTML = `i love ${arrayOfChars[13].toUpperCase()}`
-    }
-    if (input_1.value == 915) {
-     code_1.innerHTML = `i love ${arrayOfChars[14].toUpperCase()}`
-    }
-    if (input_1.value == 916) {
-     code_1.innerHTML = `i love ${arrayOfChars[15].toUpperCase()}`
-    }
-    if (input_1.value == 917) {
-     code_1.innerHTML = `i love ${arrayOfChars[16].toUpperCase()}`
-    }
-    if (input_1.value == 918) {
-     code_1.innerHTML = `i love ${arrayOfChars[17].toUpperCase()}`
-    }
-    if (input_1.value == 919) {
-     code_1.innerHTML = `i love ${arrayOfChars[18].toUpperCase()}`
-    }
-    if (input_1.value == 920) {
-     code_1.innerHTML = `i love ${arrayOfChars[19].toUpperCase()}`
-    }
-    if (input_1.value == 921) {
-     code_1.innerHTML = `i love ${arrayOfChars[20].toUpperCase()}`
-    }
-    if (input_1.value == 922) {
-    code_1.innerHTML = `i love ${arrayOfChars[21].toUpperCase()}`;
-    }
-    if (input_1.value == 923) {
-     code_1.innerHTML = `i love ${arrayOfChars[22].toUpperCase()}`
-    }
-    if (input_1.value == 924) {
-     code_1.innerHTML = `i love ${arrayOfChars[23].toUpperCase()}`
-    }
-    if (input_1.value == 925) {
-     code_1.innerHTML = `i love ${arrayOfChars[24].toUpperCase()}`
-    }
-    if (input_1.value == 926) {
-     code_1.innerHTML = `i love ${arrayOfChars[25].toUpperCase()}`
-    }
-    //char
-   
+    for (let i = 901; i <= 926; i++) {
+        (em.push(i.toString()))
+        for (let j = 0; j < em.length; j++) {       
+            if (input_1.value == em[j]) {
+             code_1.innerHTML = `i love ${(arrayOfChars[j][0]).toUpperCase()}`
+            }
+        }
+   }
 }
+
+let span = document.querySelector('footer span');
+
+span.onclick = function () {
+    window.open('https://www.facebook.com/moaaz.tsh/', '_blank');
+
+}
+
